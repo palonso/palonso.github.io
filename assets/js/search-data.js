d2442828490a883972b7ea984e0a7c7693f7f2b0
palonso <pablo.alonso@upf.edu>
2026-06-11 19:09:32 +0000

    Deploying to gh-pages from @ palonso/palonso.github.io@d534ac5d8c53717e53051e603f987c6982edf4e6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -46,7 +46,12 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "projects-project-10",
+          title: 'project 10',
+          description: "A project with an introduction section",
+          section: "Projects",handler: () => {
+              window.location.href = "/palonso.github.io/projects/10_project/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
